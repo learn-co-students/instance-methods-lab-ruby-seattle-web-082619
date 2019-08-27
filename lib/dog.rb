0326ba2cd7f270782959071f5241c70a3fd7c02f
renburnett <renardburnett@gmail.com>
2019-08-27 12:55:53 -0700

    added instance method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 class Dog
-    def bark()
-        puts "Woof!"
+    def name=(dog_name)
+        @this_dogs_name = dog_name
     end
-    def sit()
-        puts "The Dog is sitting"
+    def name
+        @this_dogs_name
     end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
